2d6b50adc3f71032783d03aebd0b033ea93c30a7
yuriprym <samuel.young.103@gmail.com>
2018-02-15 11:37:58 -0500

    add in a main file

---
+++ InfluenzaMachineLearning.py
@@ -0,0 +1,15 @@
+##### title ########
+## Authors : Samuel Young, Thomas Raymond, Bethany Yachuw
+## starting date: 02/15/18
+## last date update: 02/15/18
+## objective:
+## 
+#############################
+
+
+
+
+def main():
+    print "the next line"
+
+main()
