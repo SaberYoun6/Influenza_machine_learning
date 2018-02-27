ba692f4d73e0704b4e1dd0780bdac4fa12896503
yuriprym <samuel.young.103@gmail.com>
2018-02-27 01:32:38 -0500

    I made his program object-orinated and modular

--- readseq.py
+++ readseq.py
@@ -1,7 +1,5 @@
-#import dna_calculatortests as dna
-
+import DNA_calculators as dna
 import itertools
-#import protein_calculatorstest as prt
 import sys
 ### variable ####
 #No Global
@@ -15,20 +13,32 @@ class method():
     # this should allow us to read in a file and output the result that we want
         ####variables
         head_seqs={}
-        
+        header=[]
+        seqs=[]
+        gene=""
         ###
         with open(filename) as f:
             #we are reading in the file
 #the next few lines are used to obtain both the header of the file and then the seq of the file as well 
-            fh= f.readline()
-            headerfile=fh.split("\n")
-            if(headerfile().startswith('>')):
-                header = headerfile 
-            else:
-               headerfile =seq
-            for header, seq in enumerate(f):
-                head_seqs['header'+str(header)]= [seq] 
-            return head_seqs
+            fh= f.readlines()
+# we are reading file line by line
+            for line in fh:
+#this is returning the fileheader and then appending it by line
+                if(line[0] == ('>')):
+                    header.append(line)
+#this is 
+                    if len(gene) != 0:
+                        seqs.append(gene)
+                        gene= ''
+                else:
+                    gene += line.strip()
+        seqs.append(gene)
+
+        strainprofiles={}
+        for i in range(0,len(seqs)):
+            strainprofiles[header[i]]=dna.fullProfile(seqs[i])
+
+        return strainprofiles
 
 
 
@@ -42,7 +52,5 @@ def main():
     name =sys.argv[1]
     seqdata=mthd.read_file(name)
     print seqdata
-    #        print(key + ","+ value + "\n")
-    #dna.fullProfile(seqdata)
 
 main()
