fd04f6f1ab393d4f4bba2992139a886713ce7c7a
yuriprym <samuel.young.103@gmail.com>
2018-02-26 23:18:32 -0500

    readseq is currently printing out a memory address and a bunch of gibberish update

--- readseq.py
+++ readseq.py
@@ -1,26 +1,25 @@
-import dna_calculatortests as dna
+#import dna_calculatortests as dna
+
+import itertools
 import protein_calculatorstest as prt
 import sys
 
 class method():
+    #def __init__(self):
 
-
-    def __init__(self):
-    @staticmethod
     def read_file(self,filename):
-# this should allow us to read in a file and output the result that we want
+    # this should allow us to read in a file and output the result that we want
+        head_seqs={}
         with open(filename) as f:
             #we are reading in the file
-            seq=f.read().split()
-            seq=seq.split()
-
-            return seq
-    def header(datafile):
-#this should return the header and the sequence data to read in for dna_calculatortest
-        data= datafile
-        heads=[]
-        
-         
+            headerfile=f.next().strip()
+            headerfile=headerfile.split('\n')
+            seq=f.next().strip()
+            seq=seq.split('\n')
+            # this should return all the value in head_seq{}
+            for k,v in itertools.groupby(headerfile,lambda x: x[len(seq)]):
+                    head_seqs.setdefault(k,[]).append(v)
+            return head_seqs
 
 
 
@@ -31,6 +30,9 @@ class method():
 
 def main():
     mthd= method()
-    name =sys.arg[2]
+    name =sys.argv[1]
     seqdata=mthd.read_file(name)
-    
+    print seqdata
+ #   dna.fullProfile(seqdata)
+
+main()
