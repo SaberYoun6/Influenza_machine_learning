fd7c77e74647d8096cd4eafc6a79571bc0bc868f
yuriprym <samuel.young.103@gmail.com>
2018-02-27 02:22:00 -0500

    we add in protien already it is now just waiting for someone to make it a method so thatwe can use the protein sturturces

--- readseq.py
+++ readseq.py
@@ -9,7 +9,7 @@ import sys
 class method():
     #def __init__(self):
 
-    def read_file(self,filename):
+    def read_dna_file(self,filename):
     # this should allow us to read in a file and output the result that we want
         ####variables
         head_seqs={}
@@ -26,7 +26,7 @@ class method():
 #this is returning the fileheader and then appending it by line
                 if(line[0] == ('>')):
                     header.append(line)
-#this is 
+#this is used to get the length of the genes in which we are going to uses and then to append them to the file 
                     if len(gene) != 0:
                         seqs.append(gene)
                         gene= ''
@@ -39,8 +39,36 @@ class method():
             strainprofiles[header[i]]=dna.fullProfile(seqs[i])
 
         return strainprofiles
+'''   def read_protein_file(self,filename):
+    # this should allow us to read in a file and output the result that we want
+        ####variables
+        head_seqs={}
+        header=[]
+        seqs=[]
+        prot=""
+        ###
+        with open(filename) as f:
+            #we are reading in the file
+#the next few lines are used to obtain both the header of the file and then the seq of the file as well 
+            fh= f.readlines()
+# we are reading file line by line
+            for line in fh:
+#this is returning the fileheader and then appending it by line
+                if(line[0] == ('>')):
+                    header.append(line)
+#this is 
+                    if len(prot) != 0:
+                        seqs.append(prot)
+                        prot= ''
+                else:
+                    prot += line.strip()
+        seqs.append(prot)
 
-
+        strainprofiles={}
+        for i in range(0,len(seqs)):
+            strainprofiles[header[i]]=dna.protein_profiles(seqs[i])
+        return strainprofile
+'''
 
 
 
@@ -49,8 +77,10 @@ class method():
 
 def main():
     mthd= method()
-    name =sys.argv[1]
-    seqdata=mthd.read_file(name)
+    dnafilename =sys.argv[1]
+    #protfilename= sys.argv[2]
+
+    seqdata=mthd.read_dna_file(dnafilename)
     print seqdata
 
 main()
