aba83a2c9644d409f507884c267d8b5b92d56d86
yuriprym <samuel.young.103@gmail.com>
2018-03-01 12:10:15 -0500

    add in commandline argument that should be able to read in the data and being by graphing process

--- InfluenzaMachineLearning.py
+++ InfluenzaMachineLearning.py
@@ -5,8 +5,8 @@
 ## objective:
 ## 
 #############################
-from sklearn import svm 
-from sklearn.tree import DecisionTreeRegessor
+from sklearn import KMeans 
+from sklearn import dataset
 import matplotlib.pyplot as plt
 import dna_calculatortets as DNA
 import protien_calculatortest as prt
@@ -15,10 +15,16 @@ import numpy as np
 import sys
 import time
 class method(object):
-   def 
+    def kmeans_graph():
+        for i in line1:
+            for j in line2:
+               
+
 
 def main():
-    print "the next line"
+    print "should insert the two already created files"
+    commandline1=sys.argv[1]# Dna text1
+    commadnline2sys.argv[2] # Dna 
 
 
 main()
