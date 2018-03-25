a8ebd8142512cd308f7fd0890164b9500b1ad1d3
yuriko-tan <yuriko-tan@localhost.localdomain>
2018-03-25 14:15:56 -0400

    add in a new file called RSCU.py in which will hopefully allow us to caculate the relative synomoymous codon usage

---
+++ rscu.py
@@ -0,0 +1,21 @@
+# Samuel Young
+## RSCU.py
+## 
+##
+class mapping(object):
+    def __init__(self, triple_codon_usage,codon_used):
+        self.triple_codon_usage= triple_codon_usage
+        self.codon_used=condon_used
+    def  removing_common_compenents(seqences):
+        for seqeunce in xrange(seqences):
+            if (seqeunce.startswtih('ATG'):
+                
+            elif (seqeunce.endwith(['TAA','TAG','TGA']):
+
+               if (sequence.endwith('GTA')):
+
+               elif (sequence.startwith(['AAT',"GAT",'AGT']):
+                
+          else:
+              return seqeunce
+    def 
