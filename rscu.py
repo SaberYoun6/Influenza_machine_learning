72bd2ed8fcebbd91dca0bfe285df4d8c21b233c3
yuriko-tan <yuriko-tan@RedAlert.home>
2018-03-28 10:09:00 -0400

    So right now when I'm looking for these possible index to remove the common compenents

--- rscu.py
+++ rscu.py
@@ -1,45 +1,67 @@
+#!/etc/bin/env python 
 # Samuel Young
 ## RSCU.py
 ## 
-##
+## 
+#from biopy import seq 
 class mapping(object):
-    def __init__(self, triple_codon_usage,codon_used):
-        self.triple_codon_usage= triple_codon_usage
-        self.codon_used=condon_used
-    def  removing_common_compenents(seqences):
-        for seqeunce in xrange(seqences):
-            if (seqeunce.startswtih('ATG')):
+    #def __init__(self, triple_codon_usage,codon_used):
+    #    self.triple_codon_usage= triple_codon_usage
+    #    self.codon_used=condon_used
+    def  removing_common_comp(self,seqences):
+        
+        for seqeunce in seqences:
+            seqeunce.index('TAA')
+            seqeunce.index('TAG')
+            seqeunce.index('TGA')
+            if (seqeunce.startswith('ATG')):
                 seqeunce.strip('ATG')
-            elif (seqeunce.endwith(['TAA','TAG','TGA'])):
-                 seqeunce.strip(['TAA','TAG','TGA'])
-               if (sequence.endwith('GTA')):
-                   seqeunce.strip('GTA')
-               elif (seqeunce.startwith(['AAT',"GAT",'AGT'])):
-                   seqeunce.strip(['AAT','GAT','AGT'])
-          else:
-              return seqeunce
-    def rel_syn_cod_use(sequences,triple_codon_usage,codon_used):
-    i=1
-    while (triple_codon_usage<=i):
-         j=1
-         while codon_used <= j:
-             for X in enumerate(sequences):
-                 lengthOrignal=X[i][j]
-                 sumOrginal= sum(X[len(len(sequences)*i)][j-1]
-                 number= 1/len(sequences)
-                 rel_syn_cod_usages=lengthOrignal/number*sumOrginal
-    return rel_syn_cod_usages
+            elif (seqeunce.endswith('TAA','TAG','TGA')):
+                if (sequnce.endwith('TAA')):
+                   seqeunce.strip('TAA')
+                elif (sequnce.endwith('TAG')):
+                    seqeune.strip('TAG')
+                else:
+                    seqeunce.strip('TGA')
+                seqeunce
 
-    return rel_syn_cod_usage
-    def max_codon_usage(sequences,triple_codon_usage,codon_used):
-        for i in triple_codon_usages:
-            for j in condon_used:
-                for X in enumerate(sequences):
-                   lengthOrginal=X[i][j]
-                   lengthMax=X[i][max(len(seqeunces/3)]
-                   sumMax= Sum(X[len(len(seqeunces)*i)][max(len(seqeunces)/3)])
-                   sumOriginal=sum(X[len(len(seqeuences)*i)][j-l])
-                   w[i][j] =-(-lengthOrginal*sumMax/lengthMax*sumOrginal -lengthOrginal/lengthMax)
+            return seqeunce
+    #''''
+    #def rel_syn_cod_use(sequences,triple_codon_usage,codon_used):
+    #    i=0
+    #    while (len(sequences)/3<=i):
+    #        j=0
+    #        while (len(sequences) <= j):
+    #            number= len(sequences)
+    #            for X in enumerate(sequences):
+    #                lengthOriginal=X[i][j]
+    #                j=+1
+    #                i=+1
+    #                print (lengthOriginal)
+    #                sumOriginal= sum(X[len(len(sequences)*i)][j-1]
+    #                        rel_syn_cod_usages=lengthOriginal/((1/number)*sumOriginal)
+    #    return rel_syn_cod_usages
+
+    #def max_codon_usage(sequences,triple_codon_usage,codon_used):
+    #    for i in triple_codon_usages:
+    #        for j in condon_used:
+    #            for X in enumerate(sequences):
+    #               lengthOrginal=X[i][j]
+    #               lengthMax=X[i][(max(len(seqeunces)/3)]
+    #               sumMax= Sum(X[len(len(seqeunces)*i)][max(len(seqeunces)/3)])
+    #               sumOriginal=sum(X[len(len(seqeuences)*i)][j-l])
+    #               w[i][j] =-(-lengthOrginal*sumMax/lengthMax*sumOrginal -lengthOrginal/lengthMax)
+    #    return w 
+    # ''''
+
+def  main():
+    table=mapping()
+    seq='ATGCTGTCTCAGGCACGTGGATGGTTTGGACAAATCAGATTCAAGTCTGATCAACCTTCATACAGATCTAGAGTCTAAAGCAGTTAA' 
+    seq1='AATTTTCCCACTGCCTTAAGCCGGCTTGCCCTTTCTGCCTGTAGATCCATTGGACTGGTGCCAACGCGCAGGCATAGTTCGAGGAGAATTATCCGGGGGCAATGACAACCAGCATCTCGGGTCTTGCCCAACCCGTCTACACGCTGTTATAGCGTATCAGCGGGAACCCGGTGCCACGCGATGGAACGTCCAACTCTGGCAGGCAATTAAAGGGAACGTA'
+    table.removing_common_comp(seq)
+    seq1=seq1[::-1]
+    table.removing_common_comp(seq1)
 
 
-   return w 
+if __name__=='__main__':
+   print( main())
