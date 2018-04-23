f3413579496909b65d6f45f8866af32a8d1a0dc4
yuriprym <samuel.young.103@gmail.com>
2018-04-23 00:47:04 -0400

    the gui is coming along I'm trying to figure out how to get it to print not on command line

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #import TheBigKahuna2
 from Tkinter import *
+from tkFileDialog import * 
 import subprocess as sub
+import csv
+import os
+
 class App:
     def __init__(self,master):
 
@@ -8,39 +12,60 @@ class App:
 
         frame.pack()
 
-
         self.button = Button(
             frame, text='QUIT', fg='red', command=frame.quit
             )
         self.button.pack(side=RIGHT)
         
         self.run_the_big= Button(
-            frame, text='Run', command=self.run_the_big
+            root, text='Run', command=self.run_the_big
           )
         self.run_the_big.pack(side=LEFT)
         
 	self.obtain_file= Button(
-            frame, text="open file", command=self.obtain_file(f))## f equal files 
+            root, text="open file", command=self.obtain_file
+          )## f equal files 
         self.obtain_file.pack(side=LEFT) 
        
         self.creating_file= Button(
-            frame, text= "save file", command=self.creating_file(f))## f eqauls files
-        self.creating_file.pack(side=LEFT)	
-     
-    def obtain_file(self,filen):
-        if filen:
-            print(filen.name)
-    def creating_file(self,filename):
-        if filename:
-            print(filename.name)
-    def run_the_big(self):
-        print sub.call(["python","TheBigKahuna2.py","H1N11718.fasta","output.csv"])
+            root, text= "save file", command=self.creating_file)## f eqauls files
+        self.creating_file.pack(side=LEFT)
 
+
+    def obtain_file(self):
+        fToOpen = askopenfilename(filetypes=[("Fasta file","*.fasta"),("ALL files","*.*")])
+        if fToOpen:
+            try: 
+                print("""here it comes: self.settings["template".set(fname)""")
+            except:("Open source File", "Failed to read file\n'%s'" %fname)
+
+        fileToOpen=open(fToOpen, 'r')
+        return fileToOpen
+        Label(root, fileToOpen.read()).pack()
+        fToOpen.close()
+    def creating_file(self):
+        fToSave= asksaveasfile(mode='w',filetypes=[("comman separate values",'*.csv'),("All files","*.*")])
+        if fToSave is None:
+            return 
+        with open(fToSave,'wb') as fp:
+            fp.write('\ufeff')
+            fileToSave=csv.writer(open(fp ,quoting=csv.QUOTE_MINIMAL))
+            fileToSave.writerow(['virus type A strains', ",gc content","CpG ratio","GRAVY",["Amino"]*100])
+        buffer =open(fToSave, 'r').read()
+        print (buffer)
+        f=open(fToSave, "w")
+        f.write(buffer)
+        f.close()
+
+    def run_the_big(self):
+        print sub.call(["python","TheBigKahuna2.py" ,"H3N21617.fasta", "output3.csv"])
+            #self.obtain_file() , self.create_file()])
+
+cwd= os.getcwd()
 root =Tk()
+root.geometry("500x500")
 
 app=App(root)
-
-
 
 root.mainloop() 
 root.destroy()
