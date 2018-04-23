89479ddcc1fb0420a3e09ecd03e9e8a578cd682c
bccl <samuel.young.103@gmail.com>
2018-04-23 10:43:47 -0400

    I got the gui to take in one argument by they the save function is still giving me errors

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Influenza_machine_learning
 Its to out design the next influenza just with the use of machine learning and the capability of humans.
 
+dependency
+pip install numpy
+pip install Tkinter
 
-pip install 
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import subprocess as sub
 import csv
 import os
 
+fopen= 'H3N21718.fasta'
+fsave= 'output.csv'
 class App:
     def __init__(self,master):
 
@@ -19,7 +21,7 @@ class App:
         self.button.pack(side=RIGHT)
         
         self.run_the_big= Button(
-            root, text='Run', command=self.run_the_big
+            root, text='Run', command=self.run_the_big()
           )
         self.run_the_big.pack(side=LEFT)
         
@@ -40,28 +42,21 @@ class App:
                 print("""here it comes: self.settings["template".set(fname)""")
             except:("Open source File", "Failed to read file\n'%s'" %fname)
 
-        fileToOpen=open(fToOpen, 'r')
-        return fileToOpen
+        return fToOpen
+
         Label(root, fileToOpen.read()).pack()
         fToOpen.close()
     def creating_file(self):
         fToSave= asksaveasfile(mode='w',filetypes=[("comman separate values",'*.csv'),("All files","*.*")])
         if fToSave is None:
             return 
-        with open(fToSave,'wb') as fp:
-            fp.write('\ufeff')
-            fileToSave=csv.writer(open(fp ,quoting=csv.QUOTE_MINIMAL))
-            fileToSave.writerow(['virus type A strains', ",gc content","CpG ratio","GRAVY",["Amino"]*100])
-        buffer =open(fToSave, 'r').read()
-        print (buffer)
-        f=open(fToSave, "w")
-        f.write(buffer)
-        f.close()
-
+        return fToSave
     def run_the_big(self):
-        print sub.call(["python","TheBigKahuna2.py" ,"H3N21617.fasta", "output3.csv"])
-            #self.obtain_file() , self.create_file()])
+        openfile=str(self.obtain_file())
+        savefile= str(self.creating_file())
+        print sub.call(["python","TheBigKahuna2.py", openfile , savefile])
 
+        #print sub.call(["python","TheBigKahuna2.py" ,"H3N21617.fasta", "output3.csv"])
 cwd= os.getcwd()
 root =Tk()
 root.geometry("500x500")
