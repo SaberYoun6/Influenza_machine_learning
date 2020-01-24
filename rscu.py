1d9ca7b645a973d1c2c678fdc9c0f09962b23b8d
yuriprym <samuel.young.103@gmail.com>
2020-01-24 14:25:34 -0500

    add into rscu a function that can find the amount of codons am currently trying to implement code for figuring out how to adjust the for amino acids

--- rscu.py
+++ rscu.py
@@ -15,40 +15,61 @@ shell= 0x0034
 
 
 class mapping(object):
-    #def __init__(self,sequences, animo_acids,codon):
-    #    self.sequences= sequences
-    #    self.animo_acids= amino_acid
-    #    self.codons=condons
-    def  removing_common_comp(self,sequences,stopCodon=('TAA','TAG','TGA')):
+    def __init__(self,sequences):
+       self.sequences= sequences
+    def  removing_common_comp(self,stopCodon=('TAA','TAG','TGA')):
+        sequences =self.sequences
         seq=''
         seqs=''
         sequences.rstrip()
-        if (sequences.startswith('ATG')):
+        if sequences.startswith('ATG'):
             seq=sequences.replace('ATG','')
-        if (seq.endswith('TAA')):
+        if seq.endswith('TAA'):
               seqs = seq.replace('TAA','')
-        elif seq.endwith('TAG'):
+        elif seq.endswith('TAG'):
               seqs = seq.replace('TAG','')
         else:
               seqs = seq.replace('TGA','')
         return seqs
-    def divides_by_three(self,seq):
+
+    def divides_by_three(self,new_seq):
+        seq = new_seq
         if(len(seq)%3 == 0):
-	    return True
+            return seq
         else:
-            return False
-    #def similarity_martix(self,sequences,comparer,similar_results):
-    #    pdb.set_trace()
-    #    count = 0
-    #    for seq in sequences:
-    #        for keys,values in comparer.items():
-    #            for key,value in similar_results.items():
-    #                if (seq is values):
-    #                if (seq is key):
-    #                while True:
-    #                    if (seq.index(keys)  seq.index(key)):
-    #                        seq[sim]=+seq[comp]
-    #                        number=len(similar_results[key])
+            return None
+
+    def greater_then_hundred(self,new_seq):
+        seq = new_seq
+        if (seq == None):
+            return None
+
+        elif(len(seq)  / 100 >= 1) :
+            return seq
+
+        else:
+             return None
+
+    def similarity_martix_codon(self,new_seq,comparer):
+        
+        for i in range(0,len(new_seq),3):
+            for keys,values in comparer.items():
+                if new_seq[i:i+3] == keys:
+                    comparer[keys] += 1
+        return comparer 
+
+    def similarity_matrix_amino(self,codon,trans):
+        temp_dict={}
+        
+        for k,v in codon.items():
+            values= 0
+            for ke,va in trans.items():
+                if va == k :
+                    values += v
+                temp_dict[ke]= values
+
+        return temp_dict 
+
 
     #def rel_syn_cod_use(self,sequences,amino_acids,codons):
     #    pdb.set_trace()
@@ -74,54 +95,76 @@ class mapping(object):
     #    return w 
 
 def  main():
-    table=mapping()
     translation_dictionary= {
-            'Ala' : 'GCT' ,  'Ala': 'GCC' , 'ALA' : 'GCA' , 'Ala' : 'GCG' ,
-            'Arg' : 'CGT' ,' Arg' : 'CGC' , 'Arg' : 'CGA' , 'Arg' : 'CGG' , 'Arg' : 'AGA' , 'Arg' : 'AGG' ,
-            'Asn' : 'AAT' ,' Asn' : 'AAC' , 'Asp' : 'GAT' , 'Asp' : 'GAC' ,
-            'Cys' : 'TGT' , 'Cys' : 'TGC' , 'Gln' : 'CAA' , 'Gln' : 'CAG' , 'Glu' : 'GAA' , 'Glu ': 'GAG' ,
-            'Gly' : 'GGT' , 'Gly' : 'GGC' , 'Gly' : 'GCA' , 'Gly' : 'GCG' , 'His' : 'CAT' , 'His' : 'CAC' ,
-            'Ile' : "ATT" , 'Ile' :' ATC' , 'Ile' : 'ATA' , 
-            'Leu' : 'CTT' , 'Leu' : 'CTC' , 'Leu' : 'CTA' , 'Leu' : 'CTG' , 'Leu' : 'TTA' , 'Leu' : 'TTG' ,
-            'Lys' : 'AAA' , 'Lys' : 'AAG' , 'Met' : 'ATG' , 'Phe' : 'TTT' ,' Phe' : 'TTC' ,
-            'Pro' : 'CCT' , 'Pro' : 'CCC' , 'Pro' :' CCA' , 'Pro' : 'CCG',
-            'Ser' : 'TCT' , 'Ser' : 'TCC' , 'Ser' : 'TCA' , 'Ser' : 'TCG', 'Ser' : 'AGT' , 'Ser' : 'AGC' ,
-        'Thr' : 'CGT' , 'Thr' : 'ACC' , 'Thr' : 'ACA' , 'Thr' : 'ACG' , 'Trp' : 'TGG',
-        'Tyr' : 'TAT' , 'Tyr' : 'TAC' , 'Val' : 'GTT' , 'Val' : 'GTC' , 'Val' : 'GTA' , 'Val' : 'GTG'
+            'A' : 'GCU' , 'A' : 'GCC' , 'A' : 'GCA' , 'A' : 'GCG' ,
+            'R' : 'CGU' , 'R' : 'CGC' , 'R' : 'CGA' , 'R' : 'CGG' , 'R' : 'AGA' , 'R' : 'AGG' ,
+            'C' : 'UGU' , 'C' : 'UGC',
+            'N' : 'AAU' , 'N' : 'AAC' , 'D' : 'GAU' , 'D' : 'GAC' ,
+            'E' : 'GAA' , 'E' : 'GAG' , 'Q' : 'CAA' , 'Q' : 'CAG' ,
+            'G' : 'GGU' , 'G' : 'GGC' , 'G' : 'GGA' , 'G' : 'GGG' , 'H' : 'CAU' , 'H' : 'CAC' ,
+            'I' : "AUU" , 'I' :' AUC' , 'I' : 'AUA' , 
+            'L' : 'CUU' , 'L' : 'CUC' , 'L' : 'CUA' , 'L' : 'CUG' , 'L' : 'UUA' , 'L' : 'UUG' ,
+            'K' : 'AAA' , 'K' : 'AAG' , 'M' : 'AUG' , 'F' : 'UUU' , 'F' : 'UUC' ,
+            'P' : 'CCU' , 'P' : 'CCC' , 'P' :' CCA' , 'P' : 'CCG',
+            'S' : 'UCU' , 'S' : 'UCC' , 'S' : 'UCA' , 'S' : 'UCG', 'S' : 'AGU' , 'S' : 'AGC' ,
+            'T' : 'ACU' , 'T' : 'ACC' , 'T' : 'ACA' , 'T' : 'ACG' , 'W' : 'UGG',
+            'Y' : 'UAU' , 'Y' : 'UAC' , 'V' : 'GUU' , 'V' : 'GUC' , 'V' : 'GUA' , 'V' : 'GUG'
         }
     amino_acid_dictionary = {
-            'Ala' : 0.0 , 'Arg' : 0.0  , 'Asn' : 0.0 , 'Asp' : 0.0 , 'Cys' : 0.0 ,
-            'Gln' : 0.0 , 'Glu' : 0.0  , 'Gly' : 0.0 , 'His' : 0.0 , 'Ile' : 0.0 ,
-            'Leu' : 0.0 , 'Lys' : 0.0  , 'Met' : 0.0 , 'Phe' : 0.0 , 'pro' : 0.0 ,
-            'Ser' : 0.0 , 'Thr' : 0.0  , 'Trp' : 0.0 , 'Tyr' : 0.0 , 'Val' : 0.0
+            'A' : 0.0 , 'R' : 0.0  , 'N' : 0.0 , 'D' : 0.0 , 'C' : 0.0 ,
+            'Q' : 0.0 , 'E' : 0.0  , 'G' : 0.0 , 'H' : 0.0 , 'I' : 0.0 ,
+            'L' : 0.0 , 'K' : 0.0  , 'M' : 0.0 , 'F' : 0.0 , 'P' : 0.0 ,
+            'S' : 0.0 , 'T' : 0.0  , 'W' : 0.0 , 'Y' : 0.0 , 'V' : 0.0
             }
     codon_value_dictonary ={
-        'GCT' : 0.0 , 'GCC' : 0.0 , 'GCA' : 0.0 , 'GCG' : 0.0 ,
-        'CGT' : 0.0 , 'CGC' : 0.0 , 'CGA' : 0.0 , 'CGG' : 0.0 , 'AGA' : 0.0 , 'AGG' : 0.0 ,
-        'AAT' : 0.0 , 'AAC' : 0.0 , 'GAT' : 0.0 , 'GAC' : 0.0 ,
-        'TGT' : 0.0 , 'TGC' : 0.0 , 'CAA' : 0.0 , 'CAG' : 0.0 , 'GAA' : 0.0 , 'GAG' : 0.0 ,
-        'ATT' : 0.0 , 'ATC' : 0.0 , 'ATA' : 0.0 ,
-        'CTT' : 0.0 , 'CTC' : 0.0 , 'CTA' : 0.0 , 'CTG' : 0.0 , 'TTA' : 0.0 , 'TTG' : 0.0 ,
-        'AAA' : 0.0 , 'AAG' : 0.0 , 'ATG' : 0.0 , 'TTT' : 0.0 , 'TTC' : 0.0 ,
-        'CCT' : 0.0 , 'CCC' : 0.0 , 'CCA' : 0.0 , 'CCG' : 0.0 ,
-        'TCT' : 0.0 , 'TCC' : 0.0 , 'TCA' : 0.0 , 'TCG' : 0.0 , 'AGT' : 0.0 , 'AGC' : 0.0 ,
-        'CGT' : 0.0 , 'ACC' : 0.0 , 'ACA' : 0.0 , 'ACG' : 0.0,  'TGG' : 0.0 ,
-        'TAT' : 0.0 , 'TAC' : 0.0 , 'GTT' : 0.0 , 'GTC' : 0.0,  'GTA' : 0.0 , 'GTG' : 0.0 ,
+        'GCU' : 0.0 , 'GCC' : 0.0 , 'GCA' : 0.0 , 'GCG' : 0.0 ,
+        'CGU' : 0.0 , 'CGC' : 0.0 , 'CGA' : 0.0 , 'CGG' : 0.0 , 'AGA' : 0.0 , 'AGG' : 0.0 ,
+        'AAU' : 0.0 , 'AAC' : 0.0 , 'GAU' : 0.0 , 'GAC' : 0.0 ,
+        'UGU' : 0.0 , 'UGC' : 0.0 , 'CAA' : 0.0 , 'CAG' : 0.0 , 'GAA' : 0.0 , 'GAG' : 0.0 ,
+        'AUU' : 0.0 , 'AUC' : 0.0 , 'AUA' : 0.0 ,
+        'CUU' : 0.0 , 'CUC' : 0.0 , 'CUA' : 0.0 , 'CUG' : 0.0 , 'UUA' : 0.0 , 'UUG' : 0.0 ,
+        'AAA' : 0.0 , 'AAG' : 0.0 , 'AUG' : 0.0 , 'UUU' : 0.0 , 'UUC' : 0.0 ,
+        'CCU' : 0.0 , 'CCC' : 0.0 , 'CCA' : 0.0 , 'CCG' : 0.0 ,
+        'UCU' : 0.0 , 'UCC' : 0.0 , 'UCA' : 0.0 , 'UCG' : 0.0 , 'AGU' : 0.0 , 'AGC' : 0.0 ,
+        'CGU' : 0.0 , 'ACC' : 0.0 , 'ACA' : 0.0 , 'ACG' : 0.0,  'UGG' : 0.0 ,
+        'UAU' : 0.0 , 'UAC' : 0.0 , 'GUU' : 0.0 , 'GUC' : 0.0,  'GUA' : 0.0 , 'GUG' : 0.0 ,
         }
-    seq='ATGCTGTCTCAGGCACGTGGATGGTTTGGACAAATCAGATTCAAGTCTGATCAACCTTCATACAGATCTAGAGTCTAAAGCAGTTAA' 
-    seq1='AATTTTCCCACTGCCTTAAGCCGGCTTGCCCTTTCTGCCTGTAGATCCATTGGACTGGTGCCAACGCGCAGGCATAGTTCGAGGAGAATTATCCGGGGGCAATGACAACCAGCATCTCGGGTCTTGCCCAACCCGTCTACACGCTGTTATAGCGTATCAGCGGGAACCCGGTGCCACGCGATGGAACGTCCAACTCTGGCAGGCAATTAAAGGGAACGTA'
+    seq='ATGAATCCAAATCAAAAAATAATAACCATTGGATCAATCAGTATAGCAATCGGGATAATTAGTCTAATGTTGCAAATAGGAAATATTATTTCAATATGGGCTAGTCACTCAATCCAAACTGGAAGTCAAAACCACACTGGAATATGCAACCAAAGAATCATCACATATGAAAACAGCACCTGGGTGAATCACACATATGTTAGTATTAACAACACTAATGTTGTTGCTGGAAAGGACAAAACTTCAGTGACATTGGCCGGCAATTCATCTCTTTGTTCTATCAGTGGATGGGCTATATACACAAAAGACAACAGCATAAGAATTGGCTCCAAAGGAGATGTTTTTGTCATAAGAGAACCTTTCATATCATGTTCTCATTTGGAATGCAGGACCTTTTTTCTGACCCAAGGTGCTCTATTAAATGACAAACATTCAAATGGAACCGTTAAGGACCGAAGTCCTTATAGGGCCCTAATGAGCTGTCCTCTAGGTGAAGCTCCGTCTCCATACAATTCAAAGTTTGAATCAGTTGCATGGTCAGCAAGCGCATGCCATGATGGCATGGGCTGGTTAACAATCGGAATTTCTGGTCCAGACAATGGAGCTGTGGCTGTACTAAAATACAACGGAATAATAACTGAAACCATAAAAAGTTGGAAAAAGCGAATATTGAGAACACAAGAGTCTGAATGTGTCTGTGTGAACGGGTCATGTTTCACCATAATGACCGATGGCCCGAGTAATGGGGCCGCCTCGTACAAAATCTTCAAGATCGAAAAGGGGAAGGTTACTAAATCAATAGAGTTAAATGCACCCAATTTTCATTATGAGGAATGTTCCTGTTACCCAGACACTGGCACAGTGATGTGTGTATGCAGGGACAACTGGCATGGTTCAAATCGACCTTGGGTGTCTTTTAATCAAAACTTGGATTATCAAATAGGATACATCTGCAGTGGAGTGTTCGGTGACAATCCGCGTCCCAAAGATGGAAAGGGCAGCTGTAATCCAGTGACTGTTGATGGAGCAGACGGGGTTAAGGGGTTTTCATACAAATATGGTAATGGTGTTTGGATAGGAAGAACTAAAAGTAACAGACTTAGAAAGGGGTTTGAGATGATTTGGGATCCTAATGGATGGACAGATACCGACAGTGATTTCTCAGTGAAACAGGATGTTGTGGCAATAACTGATTGGTCAGGGTACAGTGGAAGTTTCGTTCAACATCCTGAGTTAACAGGATTGGACTGTATAAGACCTTGCTTCTGGGTTGAGTTAGTCAGAGGACTGCCTAGAGAAAATACAACAATCTGGACTAGTGGGAGCAGCATTTCTTTTTGTGGCGTTGATAGTGATACTGCAAACTGGTCTTGGCCAGACGGTGCTGAGTTGCCGTTCACCATTGACAAGTAG'
+    seq1='ATGAAAGTAAAACTACTGGTCCTATTATGCACATTTACAGCTACATATGCAGACACAATATGTATCGGCTACCATGCCAACAACTCAACCGACACTGTTGACACAGTACTTGAAAAGAATGTGACAGTGACACACTCTGTCAACCTGCTTGAGGACAACCACAATGGAAAACTATGTCTATTAAAAGGAATAGCCCCATTACAATTGGGTAACTGCAGCGTTGCCGGGTGGATCTTAGGAAACCCAGAATGCGAATTACTGATTTCCAAGGAGTCATGGTCCTACATTGTAGAAAAACCAAATCCTGAGAATGGAACATGTTACCCAGGGCATTTCGCCGACTATGAGGAACTGAGGGAGCAATTGAGTTCAGTATCTTCATTTGAGAGGTTCGAAATATTCCCCAAAGAAAGCTCATGGCCCAACCACACCGTAACCGGAGTATCCGCATCATGCTCCCATAATGGGGAAAGCAGCTTTTACAAAAATTTGCTATGGCTGACGGGAAAGAATGGTTTGTACCCAAACCTGAGCAAGTCCTATGCAAACAACAAAGAGAAAGAAGTCCTCGTACTATGGGGTGTTCATCACCCGCCAAACATAGGTGACCAAATGACCCTCTATCATAAAGAAAATGCTTATGTCTCTGTAGTGTCTTCACATTATAGCAGAAAATTCACCCCAGAAATAGCCAAAAGACCCAAAGTAAGAGATCAAGAAGGAAGAATCAACTACTACTGGACTCTGCTTGAACCCGGGGATACAATAATATTTGAGGCAAATGGAAATCTAATAGCGCCAAGATATGCTTTCGCACTGAGTAGAGGCTTTGGATCAGGAATCATCAACTCAAATGCACCAATGGATGAATGTGATGCGAAGTGCCAAACACCTCAGGGAGCTATAAACAGCAGTCTTCCTTTCCAGAATGTACACCCAGTCACAATAGGAGAATGTCCAAAGTATGTCAGGAGTGCAAAATTAAGGATGGTTACAGGACTAAGGAACATCCCATCCATTCAATCCAGAGGTTTGTTTGGAGCCATTGCCGGTTTCATTGAAGGGGGGTGGACTGGAATGGTAGATGGTTGGTATGGTTATCATCACCAGAATGAGCAAGGATCTGGCTATGCTGCAGATCAAAAAAGCACACAAAATGCCATTAATGGGATTACAAACAAGGTGAATTCTGTAATTGAGAAGATGAACACTCAATTCACAGCTGTGGGCAAAGAATTCAACAAATTGGAAAGAAGGATGGAAAACTTAAATAAAAAAGTTGATGACGGGTTTATAGACGTTTGGACATATAATGCAGAACTGTTGGTTCTACTGGAAAATGAAAGAACTTTGGATTTCCATGACTCCAATGTGAAGAATTTGTATGAGAAAGTAAAAAATCAATTAAAGAATAATGCCAAAGAAATAGGAAATGGGTGTTTTGAATTTTATCACAAGTGTAACGATGAATGCATGGAGAGTGTAAAAAATGGAACTTATGACTATCCAAAATATTCCGAAGAATCAAAGTTAAGCAGGGAGAAAATTGATGGAGTGAAATTGGAATCAATGGGAGTCTATCAGATTCTGGCGATCTACTCAACAGTCGCCAGTTCTCTGGTTCTTTTGGTCTCCCTGGGGGCAATCAGCTTCTGGATGTGTTCCAATGGGTCTTTGCAGTGTAGAATATGCATCTAA'
     
-    common_varies=table.removing_common_comp(seq)
-    seq1=seq1[::-1]
-    not_optimal= table.removing_common_comp(seq1)
-    print (common_varies)
-    print (not_optimal)
-    babel=table.divides_by_three(common_varies)
-    non_optimal=table.divides_by_three(not_optimal)
-    #babel=table.similarity_martix(seq,translation_dictionary,amino_acid_dictionary)
-    print babel
-    print non_optimal 
+    table=mapping(seq)
+    common_varies=table.removing_common_comp()
+    common_varies = common_varies.replace('T','U')
+    
+    tabling=mapping(seq1)
+    not_optimal= tabling.removing_common_comp()
+    not_optimal= not_optimal.replace('T','U')
+    
+    print ("%s \n",common_varies)
+    print ("%s \n",not_optimal)
 
+
+    babel=(table.divides_by_three(common_varies))
+    non_optimal=(tabling.divides_by_three(not_optimal))
+    
+    babel_tower=table.similarity_martix_codon(common_varies,codon_value_dictonary)
+
+
+
+
+    print("%s \n",babel)
+    print("%s \n",non_optimal)
+    bablyon_tower= table.similarity_matrix_amino(babel_tower,translation_dictionary)
+
+    tower=(table.greater_then_hundred(common_varies))
+    def_optimal=table.greater_then_hundred(non_optimal)
+
+    print("%s \n",tower)
+    print("%s \n",def_optimal)
+
+    print(babel_tower)
+    
+    print(bablyon_tower)
 
 if __name__=='__main__':
     main()
